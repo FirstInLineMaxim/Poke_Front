d178713ef16ce4b110e207411e3eb38bbdadf76d
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2022-11-29 20:31:08 +0100

    cards responsive

--- src/PokeCard/card.jsx
+++ src/PokeCard/card.jsx
@@ -3,12 +3,23 @@ import data from '../data/poke.json'
 import './card.css'
 
 export default function Card() {
+    function changeNumber (num){
+        if (num.toString().length === 1){
+            return `00`+num
+        }else if ( num.toString().length === 2){
+            return '0'+num
+        }else if(num.toString().length === 3){
+            return num
+        }
+
+    }
 
   return (
     <>
     <div className='Card_Display'>
     {data.map(ele => {return( <div className='Card_Container'>
-            <img src="" alt=""/>
+                    {/* link options i found pokedex/ detail or full  */}
+            <img src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${changeNumber(ele.id)}.png`} alt={ele.name.english}/>
             {/* Description */}
             <div className='Descript_Container'>
                 <p>#{ele.id}</p>
