82732231cf6db053f680790b7cfba449cb5faba7
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2022-12-01 14:43:44 +0100

    hero

--- src/HeroPage/Hero.js
+++ src/HeroPage/Hero.js
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import './hero.css'
 export default function Hero() {
   return (
     <>
-    
+    <div className='Hero_Container'>
+        
+    </div>
     </>
   )
 }
