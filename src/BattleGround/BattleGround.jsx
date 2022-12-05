b4c8eacef0d7065a8da5873ddd001904979c6f4b
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2022-12-05 19:00:23 +0100

    center stuff

--- src/BattleGround/BattleGround.jsx
+++ src/BattleGround/BattleGround.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import useCombat from '../combat/combat'
+import '../combat/combat.css'
 
 export default function BattleGround() {
 
@@ -11,9 +12,11 @@ export default function BattleGround() {
     let {id} = useParams()
   return (
     <>
+    <div className='battleGround_Container'>
     <h1>Just the Battle nothing to see here </h1>
     <p>Selected Pokemon id = {id}</p>
     <h2>{useCombat({id,enemy:random()})}</h2>
+    </div>
     </>
   )
 }
