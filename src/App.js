f237ddfb78448d06667dde12edf07b07da190adc
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2022-12-05 10:08:32 +0100

    all good

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Collection from './PokeCard/collection';
 import { Routes, Route } from "react-router-dom";
+import Hero from './HeroPage/Hero';
 
 function App() {
   return (
     <>
     <Routes>
-      <Route path="/" element={<Collection/>}/>
+      <Route path="/" element={<Hero/>}/>
       <Route path="/Choose" element={<Collection/>}/>
     </Routes>
     </>
--- src/HeroPage/Hero.js
+++ src/HeroPage/Hero.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React,{useState} from 'react'
+import Collection from '../PokeCard/collection'
 import './hero.css'
 export default function Hero() {
+    const [selectScreen, setSelectScreen] = useState()
+    function popup(){
+        !selectScreen ? setSelectScreen(<div className='Popup'><Collection className="Collection_Hero"/></div>)
+        : setSelectScreen(null)
+            
+        
+    }
   return (
     <>
     <div className='Hero_Container'>
-        
+        {selectScreen && selectScreen }
+        <button onClick={popup} className='Fight_Button'>Fight</button>
     </div>
     </>
   )
