6036175bba9b9bc2ab0fc8c19b41d1984b5080b8
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2022-12-08 19:35:41 +0100

    stuff

--- src/ChooseStarter/startercard.jsx
+++ src/ChooseStarter/startercard.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function StarterCard({ data,pokeid }) {
@@ -25,33 +26,64 @@ export default function StarterCard({ data,pokeid }) {
     )}.png`;
   };
 
+  const[display, setDisplay]= useState(<PokeCard/>)
   // TODO:make filter
+
   function showInfo(){
+      setDisplay(<PokeInfo/>)
+  }
+  function showCard(){
+    setDisplay(<PokeCard/>)
+  }
+
+
+  function PokeCard (){
+    return(<div className="Card_Body">
+    <div key={data.id} className="Card_Container">
+      <span onClick={showInfo}className="info"><img src="https://www.pngitem.com/pimgs/m/195-1951784_info-icon-svg-transparent-background-information-icon-hd.png"></img></span>
+      <Link to={`Battle/${pokeid}`}>
+      <img src={url(data.id)} alt={data.name.english} />
+      {/* Description */}
+      <div className="Descript_Container">
+        <p>#{changeNumber(data.id)}</p>
+        <div>
+          <h3>{data.name.english}</h3>
+          <div className="Types_Container">
+            {data.type.map((data, i) => (
+              <span key={i} className={data}>
+                {data}
+              </span>
+            ))}
+          </div>
+        </div>
+      </div></Link>
+    </div>
+  </div>)
+  }
+
+  function PokeInfo (){
+    return(<div className="Card_Body">
+    <div key={data.id} className="Card_Container">
+      <span onClick={showCard}className="info"><img src="https://www.pngitem.com/pimgs/m/195-1951784_info-icon-svg-transparent-background-information-icon-hd.png"></img></span>
+      <Link to={`Battle/${pokeid}`}>
+      <img src={url(data.id)} alt={data.name.english} />
+      {/* Description */}
+      <div className="Descript_Container">
+        <p>#{changeNumber(data.id)}</p>
+        <div>
+          <h3>Stats</h3>
+          <h5>Attack:{data.base.Attack}</h5>
+          <h5>Defense:{data.base.Defense}</h5>
+          <h5>HealthPoints:{data.base.HP}</h5>
+        </div>
+      </div></Link>
+    </div>
+  </div>)
   }
 
   //so only mounts if data is present
   if (data)
     return (
-      <div className="Card_Body">
-        <div key={data.id} className="Card_Container">
-          <span onclick={showInfo}className="info"><img src="https://www.pngitem.com/pimgs/m/195-1951784_info-icon-svg-transparent-background-information-icon-hd.png"></img></span>
-          <Link to={`Battle/${pokeid}`}>
-          <img src={url(data.id)} alt={data.name.english} />
-          {/* Description */}
-          <div className="Descript_Container">
-            <p>#{changeNumber(data.id)}</p>
-            <div>
-              <h3>{data.name.english}</h3>
-              <div className="Types_Container">
-                {data.type.map((data, i) => (
-                  <span key={i} className={data}>
-                    {data}
-                  </span>
-                ))}
-              </div>
-            </div>
-          </div></Link>
-        </div>
-      </div>
+      [display]
     );
 }
