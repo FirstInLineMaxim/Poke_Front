15a552639715ff92c37c57e70700185e932ccac1
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2022-12-09 13:28:06 +0100

    fixed post

--- src/UserCreate.js
+++ src/UserCreate.js
@@ -1,18 +1,35 @@
-import axios from 'axios'
-import React,{useState} from 'react'
+import axios from "axios";
+import React, { useState } from "react";
 
-export default function UserCreate({score,pokemon}) {
-    const [username,setUsername]= useState()
-    function gameEnd (e){
-    e.preventDefault(); 
-    axios.post('https://poke-api-f2zt.onrender.com/leaderboard/create',{score:score , username:username,pokemon:pokemon._id})}
+export default function UserCreate({ score, pokemon }) {
+  const [username, setUsername] = useState();
+  async function gameEnd(e) {
+    e.preventDefault();
+    const {status} = await axios.post("https://poke-api-f2zt.onrender.com/leaderboard/create", {
+      score: score,
+      username: username,
+      pokemon: pokemon._id,
+    });
+   if(status === 200){
+    window.location.replace('/leaderboard')
+   }
+  }
   return (
     <>
-    <form >
+      <form>
         <h2>Your Score this Round :{score}</h2>
-        <input type='text' onChange={(e)=>{setUsername(e.target.value)}}placeholder="What is Your Name?" name='username'/>
-        <button type='submit' onClick={(e)=> gameEnd(e)}>Submit</button>
-    </form>
-        </>
-  )
+        <input
+          type="text"
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+          placeholder="What is Your Name?"
+          name="username"
+        />
+        <button type="submit" onClick={(e) => gameEnd(e)}>
+          Submit
+        </button>
+      </form>
+    </>
+  );
 }
