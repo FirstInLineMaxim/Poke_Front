f225702779e51ba20baf1b3641564bda1f8510b2
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2023-02-01 14:32:53 +0100

    styling

--- src/HeroPage/Hero.js
+++ src/HeroPage/Hero.js
@@ -1,30 +1,38 @@
-import React,{useState} from 'react'
-import { Link } from 'react-router-dom'
-import Starter from '../ChooseStarter/starter'
-import './hero.css'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Starter from "../ChooseStarter/starter";
+import "./hero.css";
 export default function Hero() {
-    const [selectScreen, setSelectScreen] = useState()
-    function popup(){
-        // !selectScreen ? setSelectScreen(<div className='Popup'><Collection className="Collection_Hero"/></div>)
-        // : setSelectScreen(null)
-        !selectScreen ? setSelectScreen(<div className='Popup'><Starter/></div>)
-        : setSelectScreen(null)
-        
-    }
+  const [selectScreen, setSelectScreen] = useState();
+  function popup() {
+    // !selectScreen ? setSelectScreen(<div className='Popup'><Collection className="Collection_Hero"/></div>)
+    // : setSelectScreen(null)
+    !selectScreen
+      ? setSelectScreen(
+          <div className="Popup">
+            <Starter />
+          </div>
+        )
+      : setSelectScreen(null);
+  }
   return (
     <>
-    <div className='Hero_Container'>
+      <div className="Hero_Container">
         {/* NAVIGATION */}
-        <div className='TopNav'>
-        <Link className='' to="choose"><button>Pokedex</button></Link>
-        <Link className='' to="leaderboard"><button>Leaderboard</button></Link>
+        <div className="TopNav">
+          <Link className="" to="choose">
+            <button className="header_btn">Pokedex</button>
+          </Link>
+          <Link className="" to="leaderboard">
+            <button className=" header_btn">Leaderboard</button>
+          </Link>
         </div>
-        <button onClick={popup} className='Fight_Button'>Fight</button>
-      {/* SETS THE POPUPSCREEN HTML CODE IF ITS PRESENTED */}
-        {selectScreen && selectScreen }
-        
-
-    </div>
+        <button onClick={popup} className="Fight_Button">
+          Fight
+        </button>
+        {/* SETS THE POPUPSCREEN HTML CODE IF ITS PRESENTED */}
+        {selectScreen && selectScreen}
+      </div>
     </>
-  )
+  );
 }
--- src/Leaderboard/leaderboard.jsx
+++ src/Leaderboard/leaderboard.jsx
@@ -1,61 +1,76 @@
-import axios from 'axios'
-import React, { useState, useEffect } from 'react'
-import './leaderboard.css'
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import "./leaderboard.css";
 
 export default function Leaderboard() {
-    const [leaderboard, setLeaderboard] = useState()
-    const [data, setData] = useState()
+  const [leaderboard, setLeaderboard] = useState();
+  const [data, setData] = useState();
 
-    useEffect(() => {
-        axios.get('https://poke-api-f2zt.onrender.com/leaderboard').then(data => setLeaderboard(data.data)).catch(err=>console.log(err))
-        axios.get('https://poke-api-f2zt.onrender.com/api/v1/pokemon').then(data => setData(data.data)).catch(err=>console.log(err))
-    }, [])
-    function changeNumber(num) {
-        if (num.toString().length === 1) {
-            return `00` + num;
-        } else if (num.toString().length === 2) {
-            return "0" + num;
-        } else if (num.toString().length === 3) {
-            return num;
-        }
+  useEffect(() => {
+    axios
+      .get("https://poke-api-f2zt.onrender.com/leaderboard")
+      .then((data) => setLeaderboard(data.data))
+      .catch((err) => console.log(err));
+    axios
+      .get("https://poke-api-f2zt.onrender.com/api/v1/pokemon")
+      .then((data) => setData(data.data))
+      .catch((err) => console.log(err));
+  }, []);
+  function changeNumber(num) {
+    if (num.toString().length === 1) {
+      return `00` + num;
+    } else if (num.toString().length === 2) {
+      return "0" + num;
+    } else if (num.toString().length === 3) {
+      return num;
     }
-    function findPokemon(id) {
-        if (data) {
-            // eslint-disable-next-line eqeqeq
-            const found = data.find(ele => ele._id == id)
-            if (found) {
-                return { image: findImage(found), pokemonName: found.name.english }
-            }
-        }
-
+  }
+  function findPokemon(id) {
+    if (data) {
+      // eslint-disable-next-line eqeqeq
+      const found = data.find((ele) => ele._id == id);
+      if (found) {
+        return { image: findImage(found), pokemonName: found.name.english };
+      }
     }
-    function findImage(found) {
-        return `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${changeNumber(
-            found.id
-        )}.png`
-    }
+  }
+  function findImage(found) {
+    return `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${changeNumber(
+      found.id
+    )}.png`;
+  }
 
-    return (
-        <table className='HighScore'>
-            <thead>
-                <tr className='rows_flex'>
-                    <th>Username</th>
-                    <th>Pokemon</th>
-                    <th>Score</th>
-                </tr>
-            </thead>
-            <tbody className='display_reverse'>
-                {data ? leaderboard && leaderboard.map(function (ele, i) {
-                    const { image, pokemonName } = findPokemon(ele.pokemon)
-                    return (
-                        <tr key={i} className='rows_flex'>
-                            <td>{ele.username}</td>
-                            <td><img className="leader_poke" src={image} alt={pokemonName} />{pokemonName}</td>
-                            <td>{ele.score}</td>
-                        </tr>)
-
-                }) : <tr className='Center_Pokeball'><td className="loader_spinner"></td></tr>}
-            </tbody>
-        </table>
-    )
+  return (
+    <table className="HighScore">
+      <thead>
+        <tr className="rows_flex">
+          <th>Username</th>
+          <th>Pokemon</th>
+          <th>Score</th>
+        </tr>
+      </thead>
+      <tbody className="display_reverse">
+        {data ? (
+          leaderboard &&
+          leaderboard.map(function (ele, i) {
+            const { image, pokemonName } = findPokemon(ele.pokemon);
+            return (
+              <tr key={i} className="rows_flex">
+                <td className="username">{ele.username}</td>
+                <td className="leader_poke">
+                  <img src={image} alt={pokemonName} />
+                  <p>{pokemonName}</p>
+                </td>
+                <td>{ele.score}</td>
+              </tr>
+            );
+          })
+        ) : (
+          <tr className="Center_Pokeball">
+            <td className="loader_spinner"></td>
+          </tr>
+        )}
+      </tbody>
+    </table>
+  );
 }
